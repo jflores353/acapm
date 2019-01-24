094ac40fdcbfa2a781ac6dffa2c3ab7f55b3af91
Gabe <jflores353@gmail.com>
2019-01-23 19:32:41 -0600

    first commit to ACAPM

---
+++ bin/index.js
@@ -0,0 +1,15 @@
+const readline = require(' readline ');
+
+const rl = readline.createInterface({
+   input: process.stdin,
+   output: process.stdout
+});
+
+const inputs = {
+    input : ''
+}
+
+rl.question("Ask for 5 inputs"), function() {
+
+}
+
---
+++ index.js
@@ -0,0 +1,2 @@
+#!/usr/bin/env node
+console.log("ACA package manager");
